43b54b784fac9dc8c5f4017486f828124794afa7
mctbs <martin.cernak@payout.one>
2023-05-09 19:57:10 +0200

    Remove unused comment

--- lib/authex/application.ex
+++ lib/authex/application.ex
@@ -1,8 +1,4 @@
 defmodule Authex.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
-  @moduledoc false
-
   use Application
 
   @impl true
@@ -10,12 +6,8 @@ defmodule Authex.Application do
     children = [
       AuthexWeb.Endpoint,
       Authex.Repo
-      # Starts a worker by calling: Authex.Worker.start_link(arg)
-      # {Authex.Worker, arg}
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Authex.Supervisor]
     Supervisor.start_link(children, opts)
   end
